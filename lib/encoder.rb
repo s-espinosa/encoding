625e42b4cb1bf047d83d6c4c0a071bf7adbe3091
Sal Espinosa <sespinos@gmail.com>
2020-09-04 21:34:20 -0600

    Rename to make space for new method

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -4,11 +4,11 @@ class Encoder
     current_character = input[0]
     current_count = 0
 
-    encoded, current_character, current_count = add_characters(input, encoded, current_character, current_count)
+    encoded, current_character, current_count = parse_body(input, encoded, current_character, current_count)
     add_final_characters(encoded, current_count, current_character)
   end
 
-  def self.add_characters(input, encoded, current_character, current_count)
+  def self.parse_body(input, encoded, current_character, current_count)
     input.each_char do |letter|
       if letter == current_character
         current_count += 1

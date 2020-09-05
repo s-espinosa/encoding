f758b5f0034a1579359bd17b8468b78cc081731f
Sal Espinosa <sespinos@gmail.com>
2020-09-04 21:22:49 -0600

    Smaller methods

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -4,6 +4,11 @@ class Encoder
     current_character = input[0]
     current_count = 0
 
+    encoded, current_character, current_count = add_characters(input, encoded, current_character, current_count)
+    add_final_characters(encoded, current_count, current_character)
+  end
+
+  def self.add_characters(input, encoded, current_character, current_count)
     input.each_char do |letter|
       if letter == current_character
         current_count += 1
@@ -16,13 +21,15 @@ class Encoder
         current_count = 1
       end
     end
+    [encoded, current_character, current_count]
+  end
 
+  def self.add_final_characters(encoded, current_count, current_character)
     if current_count == 1
       encoded = encoded + current_character
     else
       encoded = encoded + current_count.to_s + current_character
     end
-
     encoded
   end
 end
